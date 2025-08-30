7d6096d9bd54050eae8b1af68348c9318a92e898
Vaibhav Dhiman <vaibhavdhiman108@gmail.com>
2025-08-31 01:07:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,6 @@ form.addEventListener('submit', function (e) {
   alert(`Thanks for contacting me, ${name}! I will get back to you soon.`);
   form.reset();
 });
+
+<script defer data-domain="vaibhav123868.github.io/portfolio." src="https://plausible.io/js/script.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+<script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
